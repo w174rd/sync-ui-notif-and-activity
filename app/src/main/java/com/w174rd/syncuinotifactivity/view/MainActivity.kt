8eec0511f20eab0ae4034b6e8ae83d452cc08189
najib <najibbudinabdullah@gmail.com>
2025-02-20 10:51:35 +0700

    added permission request for pushnotif

--- app/src/main/java/com/w174rd/syncuinotifactivity/view/MainActivity.kt
+++ app/src/main/java/com/w174rd/syncuinotifactivity/view/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.w174rd.syncuinotifactivity.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.w174rd.syncuinotifactivity.R
@@ -20,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestNotificationPermission()
+        }
 
         onClick()
     }
@@ -43,6 +53,25 @@ class MainActivity : AppCompatActivity() {
             btnPlus.setOnClickListener {
 
             }
+        }
+    }
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.d("Permission", "Izin notifikasi diberikan ✅")
+        } else {
+            Log.d("Permission", "Izin notifikasi ditolak ❌")
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 }
